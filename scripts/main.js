f6a186607600299acbe055a56b599fa3c934bc57
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-21 11:56:19 -0500

    Created a custom event that dispatches if the ride button is clicked. Also wrote function that listens for the event then populates a blue box to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
+import { RideTicketHolders } from "./rides/RideTicketHolders.js"
 import { TicketBooth } from "./TicketBooth.js"
 
 // Import and invoke the ticket booth component function
 TicketBooth()
+RideTicketHolders()

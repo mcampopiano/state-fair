87fea3bf5e8004e5fbed434c2f91e92bf2e0c068
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-21 13:12:24 -0500

    Added a food ticket button and wrote functions that generate a pink square in the DOM when the button is clicked

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -7,11 +7,18 @@ eventHub.addEventListener("click", e => {
         eventHub.dispatchEvent(rideEvent)
     }
 })
+eventHub.addEventListener("click", e => {
+    if (e.target.id === "foodTicket") {
+        const foodEvent = new CustomEvent("foodTicketPurchased")
+        eventHub.dispatchEvent(foodEvent)
+    }
+})
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
         <button id="rideTicket">Ride Ticket</button>
+        <button id="foodTicket">Food Ticket</button>
         </div>
     `
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+import { FoodTicketHolders } from "./food/FoodTicketHolder.js"
 import { RideTicketHolders } from "./rides/RideTicketHolders.js"
 import { TicketBooth } from "./TicketBooth.js"
 
 // Import and invoke the ticket booth component function
 TicketBooth()
 RideTicketHolders()
+FoodTicketHolders()

386fd317d0eafb83d67fcbd1fbb36b489351ef91
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-21 15:10:41 -0500

    Added a games button and components that render a green square to the DOM when it is clicked

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -13,12 +13,19 @@ eventHub.addEventListener("click", e => {
         eventHub.dispatchEvent(foodEvent)
     }
 })
+eventHub.addEventListener("click", e => {
+    if (e.target.id === "gameTicket") {
+        const gameEvent = new CustomEvent("GameTicketPurchased")
+        eventHub.dispatchEvent(gameEvent)
+    }
+})
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
         <button id="rideTicket">Ride Ticket</button>
         <button id="foodTicket">Food Ticket</button>
+        <button id="gameTicket"> Ticket</button>
         </div>
     `
 }
